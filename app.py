3afb35be94a455e7f28b5d54b11e46a60cc0fb50
KritiMishra-17 <131405944+KritiMishra-17@users.noreply.github.com>
2025-05-21 20:22:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import requests
 
 def fetch_poster(movie_id):
-    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=e534171f9fb268ddae825409ead1deb7&language=en-US'.format(movie_id))
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=<my-api-key>&language=en-US'.format(movie_id))
     data = response.json()
     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
